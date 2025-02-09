66e3fdfce0ea261c031a48a4410e9c4f931068c5
Pradeep Khatri <pradeepkhatri44@live.com>
2025-02-09 22:50:57 +0530

    commit API fetch
    commit API fetch

--- ScrollIndicator-Tabs-Modal/src/components/scroll-indicator/index.jsx
+++ ScrollIndicator-Tabs-Modal/src/components/scroll-indicator/index.jsx
@@ -4,6 +4,7 @@ export default function ScrollIndicator({ url }) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [scrollPercentage, setScrollPercentage] = useState(0);
 
   async function fetchData(getUrl) {
     try {
@@ -11,8 +12,11 @@ export default function ScrollIndicator({ url }) {
       const response = await fetch(getUrl);
       const data = await response.json();
 
-      console.log(data);
-    } catch(e) {
+      if (data && data.products && data.products.length > 0) {
+        setData(data.products);
+        setLoading(false);
+      }
+    } catch (e) {
       console.log(e);
       setErrorMessage(e.message);
     }
@@ -22,5 +26,33 @@ export default function ScrollIndicator({ url }) {
     fetchData(url);
   }, [url]);
 
-  return <div></div>;
+  function handleScrollPercentage() {
+    console.log(
+      document.body.scrollTop,
+      document.documentElement.scrollTop,
+      document.documentElement.scrollHeight,
+      document.documentElement.clientHeight
+    );
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollPercentage);
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
+  console.log(data, loading);
+
+  return (
+    <div>
+      <h1>Custom Scroll Indicator</h1>
+      <div className="data-container">
+        {data && data.length > 0
+          ? data.map((dataItem) => <p>{dataItem.title}</p>)
+          : null}
+      </div>
+    </div>
+  );
 }
