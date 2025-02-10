584a355915552e46d660a2595dc9356c5b06fa7d
Pradeep Khatri <pradeepkhatri44@live.com>
2025-02-10 17:17:31 +0530

    custom modal done final
    custom modal done final

--- ScrollIndicator-Tabs-Modal/src/components/custom-modal-popup/modal-test.jsx
+++ ScrollIndicator-Tabs-Modal/src/components/custom-modal-popup/modal-test.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Modal from "./modal";
+import "./modal.css";
 
 export default function ModalTest() {
   const [showModalPopup, setShowModalPopup] = useState(false);
@@ -8,10 +9,22 @@ export default function ModalTest() {
     setShowModalPopup(!showModalPopup);
   }
 
+  function onClose() {
+    setShowModalPopup(false);
+  }
+
   return (
     <div>
       <button onClick={handleToggleModalPopup}>Open Modal Popup</button>
-      {showModalPopup && <Modal body={<div>Customized Body</div>} />}
+      {showModalPopup && (
+        <Modal
+          id={'custom-id'}
+          header={<h1>Customized Header</h1>}
+          footer={<h1>Customized Footer</h1>}
+          onClose={onClose}
+          body={<div>Customized Body</div>}
+        />
+      )}
     </div>
   );
 }
